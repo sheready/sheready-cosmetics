b3a54bd18d462e49f0c5eb46d4a613052ce432b0
sheready <csstlw@gmail.com>
2022-03-25 22:00:08 +0300

    render create form using the render-form template tag

--- products/views.py
+++ products/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.renderers import TemplateHTMLRenderer
 from products.models import Items
 from rest_framework import generics
 from .serializers import ItemsSerializer
+from rest_framework.views import APIView
 # Create your views here.
 
 class ItemsCreate(generics.CreateAPIView):
-    #API endpoint that allows creation of a new item
-    queryset = Items.objects.all()
-    serializer_class = ItemsSerializer
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "products/product_create.html"
+
+    def get(self, request):
+        product = get_object_or_404(Items)
+        serializer = ItemsSerializer(product)
+        return Response({'serializer': serializer, 'product':product})
+    
+    def post(self, request):
+        product = get_object_or_404(Items)
+        serializer = ItemsSerializer(product, data=request.data)
+        if not serializer.is_valid():
+            return Response({'serializer':serializer,'product':product})
+        serializer.save()
+        return redirect('products:item_list')
+    # #API endpoint that allows creation of a new item
+    # queryset = Items.objects.all()
+    # serializer_class = ItemsSerializer
 
 class ItemsList(generics.ListAPIView):
     #API endpoint that allows list of items
